513992089f3839c8cedf0e7d7207006c15195d3e
“Gigi <debortolg@gmail.com>
2023-01-16 08:05:13 -0800

    adding images for profile

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,84 +1,85 @@
 import React from "react";
 
 
+
 const Projects = () => {
     return (
 
-        <section class="content">
-            <div class="container">
-                <div class="row align-items-center">
-                    <div class="col">
-                        <div id="Camp-Me-Outside" class="Projects w-100">
-                            <img src="assets/images/tile.images/SC.Homepage.png" class="img img-thumbnail"
+        <Projects className="content">
+            <div className="container">
+                <div className="row align-items-center">
+                    <div className="col">
+                        <div id="Camp-Me-Outside" className="Projects w-100">
+                            <img src="assets/images/tile.images/SC.Homepage.png" className="img img-thumbnail"
                                 alt="homepage for CampMeOutside including nav and hero" />
-                            <h2 class="title">Camp Me Outside</h2>
+                            <h2 className="title">Camp Me Outside</h2>
                             <p>Collaborative project: Campsite and Fireside Story Generator</p>
-                            <a href="https://francisca-hernandez.github.io/Camp-Me-Outside/" class="btn btn-secondary"
+                            <a href="https://francisca-hernandez.github.io/Camp-Me-Outside/" className="btn btn-secondary"
                                 Target="Blank">Visit Site</a>
                         </div>
                     </div>
-                    <div class="col">
-                        <div id="Horiseon" class="Projects w-100">
-                            <img src="assets/images/tile.images/Horiseon.HP.png" class="img img-thumbnail"
+                    <div className="col">
+                        <div id="Horiseon" className="Projects w-100">
+                            <img src="assets/images/tile.images/Horiseon.HP.png" className="img img-thumbnail"
                                 alt="homepage for horiseon website including nav, hero and parts of the page content" />
-                            <h2 class="title">Horiseon</h2>
+                            <h2 className="title">Horiseon</h2>
                             <p>Individual Project: Refreacted code for an existing site and added accessibility for users.</p>
-                            <a href="https://gdebortoli.github.io/Horiseon/" class="btn btn-secondary" Target="Blank">Visit Site</a>
+                            <a href="https://gdebortoli.github.io/Horiseon/" className="btn btn-secondary" Target="Blank">Visit Site</a>
                         </div>
                     </div>
-                    <div class="col">
-                        <div id="pwgenerator" class="Projects w-100">
-                            <img src="assets/images/tile.images/PW.Gen.png" class="img img-thumbnail" alt="..." />
-                            <h2 class="title">Password Generator</h2>
+                    <div className="col">
+                        <div id="pwgenerator" className="Projects w-100">
+                            <img src="assets/images/tile.images/PW.Gen.png" className="img img-thumbnail" alt="..." />
+                            <h2 className="title">Password Generator</h2>
                             <p>Individual Project: Randomly generates a password.</p>
-                            <a href="https://gdebortoli.github.io/jS.Random.Password.Generator/" class="btn btn-secondary"
+                            <a href="https://gdebortoli.github.io/jS.Random.Password.Generator/" className="btn btn-secondary"
                                 Target="Blank">Visit Site</a>
                         </div>
                     </div>
-                    <div class="col">
-                        <div id="quizgame" class="Projects w-100">
-                            <img src="assets/images/tile.images/quizgame.png" class="img img-thumbnail" alt="..." />
-                            <h2 class="title">Coding Quiz Game</h2>
+                    <div className="col">
+                        <div id="quizgame" className="Projects w-100">
+                            <img src="assets/images/tile.images/quizgame.png" className="img img-thumbnail" alt="..." />
+                            <h2 className="title">Coding Quiz Game</h2>
                             <p>Individual Project: Timed quiz that stores user highscores and deducts time and points when an answer is incorrect.</p>
-                            <a href="https://gdebortoli.github.io/jS.API.fundamental.quizGame/" class="btn btn-secondary" Target="Blank">Visit Site</a>
+                            <a href="https://gdebortoli.github.io/jS.API.fundamental.quizGame/" className="btn btn-secondary" Target="Blank">Visit Site</a>
                         </div>
                     </div>
-                    <div class="col">
-                        <div id="runbuddy" class="Projects w-100">
-                            <img src="assets/images/tile.images/Run.Buddy.png" class="img img-thumbnail" alt="..." />
-                            <h2 class="title">Run-Buddy</h2>
+                    <div className="col">
+                        <div id="runbuddy" className="Projects w-100">
+                            <img src="assets/images/tile.images/Run.Buddy.png" className="img img-thumbnail" alt="..." />
+                            <h2 className="title">Run-Buddy</h2>
                             <p>Individual Project: A website that offers fitness training services. Styling with CSS and formatting with HTML elements.</p>
-                            <a href="https://gdebortoli.github.io/Run-Buddy/" class="btn btn-secondary" Target="Blank">Visit Site</a>
+                            <a href="https://gdebortoli.github.io/Run-Buddy/" className="btn btn-secondary" Target="Blank">Visit Site</a>
                         </div>
                     </div>
-                    <div class="col">
-                        <div id="Taskinator" class="Projects">
-                            <img src="assets/images/tile.images/Taskinator.png" class="img img-thumbnail" alt="..." />
-                            <h2 class="title">Taskinator</h2>
+                    <div className="col">
+                        <div id="Taskinator" className="Projects">
+                            <img src="assets/images/tile.images/Taskinator.png" className="img img-thumbnail" alt="..." />
+                            <h2 className="title">Taskinator</h2>
 
                             <p>Individual Project: App that creates, deletes and categorizes tasks.</p>
-                            <a href="inprogress.html" class="btn btn-secondary" Target="Blank">Visit Site</a>
+                            <a href="inprogress.html" className="btn btn-secondary" Target="Blank">Visit Site</a>
                         </div>
                     </div>
-                    <div class="col">
-                        <div id="MindOverMatter" class="Projects">
-                            <img src="assets/images/tile.images/MindOverMatter.png" class="img img-thumbnail" alt="..." />
-                            <h2 class="title">Mind-Over-Matter</h2>
+                    <div className="col">
+                        <div id="MindOverMatter" className="Projects">
+                            <img src="assets/images/tile.images/MindOverMatter.png" className="img img-thumbnail" alt="..." />
+                            <h2 className="title">Mind-Over-Matter</h2>
                             <p>Group Project: Mental health and wellness tracking application. Includes mental health resources, a daily tracker and prompts you can use to log entries associated with moods for that moment.</p>
-                            <a href="https://mind-over-matter-2.herokuapp.com/" class="btn btn-secondary" Target="Blank">Visit Site</a>
+                            <a href="https://mind-over-matter-2.herokuapp.com/" className="btn btn-secondary" Target="Blank">Visit Site</a>
                         </div>
                     </div>
-                    <div class="col">
-                        <div id="HiddenGems" class="Projects">
-                            <img src="assets/images/tile.images/HiddenGems.png" class="img img-thumbnail" alt="..." />
-                            <h2 class="title">HiddenGems</h2>
+                    <div className="col">
+                        <div id="HiddenGems" className="Projects">
+                            <img src="assets/images/tile.images/HiddenGems.png" className="img img-thumbnail" alt="..." />
+                            <h2 className="title">HiddenGems</h2>
                             <p>Group Project: Application for storing hikes, resturants, bars, and places a normal toursit may not go to. Almost like a Journal to keep track of the "Gems" you find and input their information.</p>
-                            <a href="https://stormy-retreat-53284.herokuapp.com/" class="btn btn-secondary" Target="Blank">Visit Site</a>
+                            <a href="https://stormy-retreat-53284.herokuapp.com/" className="btn btn-secondary" Target="Blank">Visit Site</a>
                         </div>
                     </div>
                 </div>
             </div>
-        </section>
+        </Projects>
     )
 }
 
---
+++ src/components/Resume/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+function Resume() {
+  return (
+    <section className="my-5">
+      <div className="my-2">
+      <p>
+        Download my <a href="https://profile.indeed.com/?hl=en_US&co=US&from=gnav-homepage&_ga=2.227390168.493233662.1673884351-1510809692.1657525204">resume!</a>
+        </p>
+        <h3>Front-end Proficiencies</h3>
+        <ul className="skills">
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>jQuery</li>
+          <li>responsive design</li>
+          <li>React</li>
+          <li>Bootstrap</li>
+          <li>Bulma</li>
+        </ul>
+        <h3>Back-end Proficiencies</h3>
+        <ul className="skills">
+          <li>APIs</li>
+          <li>Node</li>
+          <li>Express</li>
+          <li>MySQL, Sequelize</li>
+          <li>MongoDB, Mongoose</li>
+          <li>REST</li>
+          <li>GraphQL</li>
+        </ul>
+      </div>
+    </section>
+  );
+}
+
+export default Resume;
